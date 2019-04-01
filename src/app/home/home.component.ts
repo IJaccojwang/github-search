4f0241f0854e950d1b7b8cef9cc4e94afa0c0c49
Jaccojwang <danolago@gmail.com>
2019-04-01 08:56:31 +0300

    Create typescript for home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user'
+import { GithubService } from '../github.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  providers:[GithubService],
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user: User;
+  username: string;
 
-  constructor() { }
+  constructor(private githubservice:GithubService) {
+    this.githubservice.getUserData()
+    this.user = this.githubservice.user
+   }
+
 
   ngOnInit() {
   }
