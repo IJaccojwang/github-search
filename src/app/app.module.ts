b5932055142dd13520d91d37e7a3babb33e6fc35
Jaccojwang <danolago@gmail.com>
2019-04-01 09:20:23 +0300

    Add followers pipe to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import {RoutingModule} from './routing/routing.module'
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { HttpClientModule } from '@angular/common/http';
 import { RepositoriesComponent } from './repositories/repositories.component';
+import { HomeComponent } from './home/home.component';
 import { HighlightDirective } from './highlight.directive';
-import { FollowersPipe } from './followers.pipe';
+import { FollowersPipe } from './followers.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     ProfileComponent,
     RepositoriesComponent,
+    HomeComponent,
     HighlightDirective,
     FollowersPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
