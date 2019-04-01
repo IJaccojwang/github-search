440be5f69237847c7b65a32d72330932eafe1bf6
Jaccojwang <danolago@gmail.com>
2019-04-01 09:56:29 +0300

    Add Transcript code for repositories
    It will add the reposearch function and fetch repositories data from github service

--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from '../repo'
+import { GithubService } from '../github.service'
 
 @Component({
   selector: 'app-repositories',
   templateUrl: './repositories.component.html',
+  providers:[GithubService],
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+  repo: Repo;
+  username: string;
 
-  constructor() { }
+  constructor(private githubservice:GithubService) {
+    this.githubservice.getRepoData();
+    this.repo = this.githubservice.repo
+  }
+
+  userSearch() {
+    this.githubservice.userSearch(this.username);
+  }
 
   ngOnInit() {
   }
