cf96c2c461fc611a095378056f30f99b188b8f74
Jaccojwang <danolago@gmail.com>
2019-04-01 09:09:19 +0300

    Make the highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef, HostListener} from '@angular/core';
+
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef){}
+
+@HostListener("click") onClicks(){
+  this.BgColor("rgba(0,0,0,0.5)")
+}
+
+@HostListener("dblclick") onDoubleClicks(){
+  this.BgColor("inherit")
+}
+
+private BgColor(action:string){
+  this.elem.nativeElement.style.backgroundColor=action;
+
+}
 
 }
