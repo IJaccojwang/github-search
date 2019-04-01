b03ff94ba095170029e2b008726f6fb417468560
Jaccojwang <danolago@gmail.com>
2019-04-01 08:37:36 +0300

    Create user class and repo class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
 export class User {
+  constructor( public avatarimg:string, public user:string, public followers: number, public following: number, public repoNo: number, public gistNo: number, public date: any) {
+  }
 }
