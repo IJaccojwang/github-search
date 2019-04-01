d04439ee1af6be7628c72d1f10fcc34989210636
Jaccojwang <danolago@gmail.com>
2019-04-01 09:06:05 +0300

    Create typescript for profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user'
+import { GithubService } from '../github.service'
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  providers:[GithubService],
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  user: User;
+  username: string;
 
-  constructor() { }
+  constructor(private githubservice:GithubService) {
+    this.githubservice.getUserData()
+    this.user = this.githubservice.user
+   }
 
+   userSearch() {
+     this.githubservice.userSearch(this.username);
+   }
   ngOnInit() {
+
   }
 
 }
